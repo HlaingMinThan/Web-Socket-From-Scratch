2ab70fbd4010fb51d5531de6260bfeee57b2e20e
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2021-09-01 22:19:11 +0630

    Build-chat-App-lesson-3-socket-connection-setup-for-client-side-and-server-side

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let express = require("express");
-
+let socket = require("socket.io");
 /**----app setup---- */
 let app = express();
 
@@ -11,4 +11,10 @@ let server = app.listen(4000, () => {
 /**----route setup---- */
 app.get("/", (res, req) => {
   req.sendFile(__dirname + "/public/index.html");
+});
+
+/**----socket setup---- */
+let io = socket(server);
+io.on("connection", (socket) => {
+  console.log("socket connection connected" + socket.id);
 });
