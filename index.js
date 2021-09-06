3f13da412e8b3834a0b6d9e22b1bc34fb4be7d7b
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2021-09-06 09:16:50 +0630

    Build-chat-App-lesson-5-build-chat-app(event emitting)

--- index.js
+++ index.js
@@ -16,5 +16,7 @@ app.get("/", (res, req) => {
 /**----socket setup---- */
 let io = socket(server);
 io.on("connection", (socket) => {
-  console.log("socket connection connected" + socket.id);
+  socket.on("chat", (data) => {
+    io.sockets.emit("chat", data);
+  });
 });
