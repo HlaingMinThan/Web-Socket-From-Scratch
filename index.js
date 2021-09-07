f5a250aa05767300d3588188cbe8c9d6f3c9fb48
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2021-09-07 09:40:16 +0630

    Build-chat-App-lesson-6-user-is-typing-feature

--- index.js
+++ index.js
@@ -19,4 +19,7 @@ io.on("connection", (socket) => {
   socket.on("chat", (data) => {
     io.sockets.emit("chat", data);
   });
+  socket.on("typing", (name) => {
+    socket.broadcast.emit("typing", name);
+  });
 });
